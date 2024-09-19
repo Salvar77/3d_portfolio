77f69f2d455e52a5b9e5351ca255613ede08e218
Łukasz <lukaszkus77@gmail.com>
2024-09-19 16:19:25 +0200

    update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -169,7 +169,7 @@ const Contact = () => {
           width="100%"
           height="350"
           style={{ border: 0 }}
-          allowfullscreen=""
+          allowFullScreen=""
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -49,7 +49,7 @@ const ComputerCanvas = () => {
 
   return (
     <Canvas
-      frameloop="demand"
+      frameloop="always"
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
