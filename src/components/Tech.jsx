1db81117e9b2807da498af3489e02eca03c29afc
Łukasz <lukaszkus77@gmail.com>
2024-09-19 16:41:58 +0200

    update

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -6,12 +6,16 @@ import classes from "./Tech.module.scss";
 import { isAndroid } from "react-device-detect"; // Importujemy isAndroid
 
 const Tech = () => {
+  // Sprawdzamy czy nie jesteśmy na Androidzie
+  if (isAndroid) {
+    return null; // Jeśli jesteśmy na Androidzie, zwracamy null, aby pominąć renderowanie
+  }
+
   return (
     <div className={classes.techContainer}>
       {technologies.map((technology) => (
         <div className={classes.techItem} key={technology.name}>
-          {!isAndroid && <BallCanvas icon={technology.icon} />}{" "}
-          {/* Renderujemy tylko, jeśli nie Android */}
+          <BallCanvas icon={technology.icon} />
         </div>
       ))}
     </div>
