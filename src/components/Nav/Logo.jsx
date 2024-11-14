e37b6b1af396b66bb1d4700743e419750ef18069
Łukasz <lukaszkus77@gmail.com>
2024-11-14 12:54:01 +0100

    update

--- src/components/Nav/Logo.jsx
+++ src/components/Nav/Logo.jsx
@@ -5,6 +5,11 @@ import { Link, useNavigate } from "react-router-dom";
 
 const Logo = ({ showLogo }) => {
   const navigate = useNavigate();
+  const logoStyle = {
+    opacity: showLogo ? 1 : 0,
+    transform: showLogo ? "translateY(0)" : "translateY(-20px)",
+    transition: "opacity 0.5s ease-out, transform 0.5s ease-out",
+  };
 
   const handleLogoClick = () => {
     navigate("/");
@@ -12,7 +17,7 @@ const Logo = ({ showLogo }) => {
   };
 
   return (
-    <div className={classes.logo} onClick={handleLogoClick}>
+    <div style={logoStyle} className={classes.logo} onClick={handleLogoClick}>
       <img src={logo} alt="Logo firmy Search IT" />
     </div>
   );
