a0b32cfd5770ecb6a9317c547190fff87786987a
Łukasz <lukaszkus77@gmail.com>
2025-01-22 12:41:18 +0100

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import classes from "./Hero.module.scss";
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
-import { isAndroid } from "react-device-detect";
+// import { ComputersCanvas } from "./canvas";
+// import { isAndroid } from "react-device-detect";
 
 const Hero = () => {
   return (
