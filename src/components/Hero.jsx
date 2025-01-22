bd6179d3fb6133ddbd79e78f00b97adcfb48ed13
Łukasz <lukaszkus77@gmail.com>
2025-01-22 12:02:30 +0100

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,65 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
 import classes from "./Hero.module.scss";
 import { styles } from "../styles";
-import { ComputersCanvas } from "./canvas";
-import { isAndroid } from "react-device-detect";
 
 const Hero = () => {
-  const [isDesktop, setIsDesktop] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth > 992);
-    };
-
-    handleResize();
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  if (isAndroid) {
-    return (
-      <section className={classes.heroSection}>
-        <div className={classes.heroContainer}>
-          <div className={classes.centerContainer}>
-            <div className={classes.circle} />
-            <div className={classes.line} />
-          </div>
-          <div>
-            <h1 className={`${styles.heroHeadText} text-white`}>
-              Witam, jestem <span style={{ color: "#915eff" }}>Łukasz</span>
-            </h1>
-            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-              Tworzę nowoczesne strony, <br />
-              aplikacje internetowe i interfejsy użytkownika.
-            </p>
-          </div>
-        </div>
-        <div className={classes.scrollContainer}>
-          <a href="#about" aria-label="Przejdź do sekcji 'O mnie'">
-            <div className={classes.scrollLink}>
-              <motion.div
-                animate={{
-                  y: [0, 24, 0],
-                }}
-                transition={{
-                  duration: 1.5,
-                  repeat: Infinity,
-                  repeatType: "loop",
-                }}
-                className={classes.scrollDot}
-              />
-            </div>
-          </a>
-        </div>
-      </section>
-    );
-  }
-
   return (
     <section className={classes.heroSection}>
       <div className={classes.heroContainer}>
@@ -77,13 +21,10 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      {isDesktop ? (
-        <div className={classes.imageContainer}>
-          <img src="/logo3.svg" alt="Twoja firma" className={classes.image} />
-        </div>
-      ) : (
-        <ComputersCanvas />
-      )}
+
+      <div className={classes.imageContainer}>
+        <img src="/logo3.svg" alt="Logo firmy" className={classes.heroImage} />
+      </div>
       <div className={classes.scrollContainer}>
         <a href="#about" aria-label="Przejdź do sekcji 'O mnie'">
           <div className={classes.scrollLink}>
