c1abdc77ea8a846f08984ca804a397a15aac000e
Łukasz <lukaszkus77@gmail.com>
2025-01-23 16:05:47 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,21 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-const gtmId = import.meta.env.VITE_GTM_ID;
-
-if (gtmId) {
-  const script = document.createElement("script");
-  script.async = true;
-  script.src = `https://www.googletagmanager.com/gtm.js?id=${gtmId}`;
-  document.head.appendChild(script);
-
-  const noscript = document.createElement("noscript");
-  noscript.innerHTML = `
-    <iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
-      height="0" width="0" style="display:none;visibility:hidden"></iframe>`;
-  document.body.appendChild(noscript);
-}
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
