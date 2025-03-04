e857bbe5918173380b8087cee380e167bd7bb231
Łukasz <lukaszkus77@gmail.com>
2025-03-04 13:55:09 +0100

    update

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -131,7 +131,7 @@ const ProjectCard = ({
 };
 
 const Works = () => {
-  const visibleProjects = isMobile ? projects.slice(0, 4) : projects;
+  const visibleProjects = isMobile ? projects.slice(0, 3) : projects;
 
   return (
     <>
