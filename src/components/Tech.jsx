d1090d41e21112fb520041b3be144aaade68b389
Łukasz <lukaszkus77@gmail.com>
2025-04-04 12:22:40 +0200

    update

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -12,11 +12,14 @@ const Tech = () => {
 
   return (
     <div className={classes.techContainer}>
-      {technologies.map((technology) => (
-        <div className={classes.techItem} key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
+      {technologies.map((technology) => {
+        console.log(technology.name, technology.icon);
+        return (
+          <div className={classes.techItem} key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        );
+      })}
     </div>
   );
 };
